16c167be0f47ca6cd462e06a2a9b8198f9a916a0
Amtul Shafi <155689823+AMTUL-SHAFI@users.noreply.github.com>
2024-09-26 11:42:45 +0500

    Add files via upload

---
+++ js 16/app.js
@@ -0,0 +1,129 @@
+// // Chapter 15 (Array I)
+// // Q1
+
+// var emptyArr =[];
+
+// console.log( emptyArr);
+
+// // Q2
+
+// var firstNameArr =["AMTUL"];
+
+// console.log(firstNameArr);
+
+
+// // Q3
+
+// var alphabet = ["H","I","J","K"];
+
+// alert(alphabet[2]);
+
+// // Q4
+
+// var alphabet=["h","i","j","k","l","m","n","o"];
+
+//   console.log(alphabet);
+
+// //   Q5
+
+
+// var NameArr  =["AMTUL"];
+
+// var newArr =["AMTUL SHAFI"];
+
+// alert(newArr);
+
+// // Chapter 16 (Array II)
+
+// // Q1
+
+// var alphabetArr =["HELLO"];
+
+// alphabetArr.push("WORLD");
+
+// console.log(alphabetArr);
+
+
+// // Q2
+
+
+// var alphabet =["h","i","j","k"];
+
+// alphabet.pop("k");
+
+// console.log(alphabet);
+
+// // Q3
+
+// var alphabet =["h","i","j","k"];
+
+// alphabet.push("l");
+
+// console.log(alphabet);
+
+// // Chapter 16 (Array III)
+
+// // Q1
+
+// var size =["S","M","XL","XXL","XXXL"];
+
+// size.shift("XXXL");
+
+// console.log(size);
+
+
+// // Q2
+
+// var sizes = ["S","M","XL","XXL","XXXL"];
+
+// size.unshift("1","2","3");
+
+// console.log(size);
+
+// // Q3
+
+//  var birdsName =["parrot"];
+
+//  birdsName.unshift("pigeon");
+
+//  alert(birdsName[0]);
+
+//  Q4
+
+// var sizes =["S","M","XL","XXL","XXXL"];
+
+// sizes.splice(2,0,"L");
+
+// console.log(sizes);
+
+// Q5
+
+// var sizes =["S","M","XL","XXL","XXXL"];
+
+// var copy = sizes.slice(0,3);
+
+//  console.log(copy);
+
+// Q6
+
+// var pets = ["dog","cat","ox","duck","frog"];
+
+// pets.splice(1,3,"deer","lion",);
+
+// console.log(pets);
+
+// Q7
+
+// var pets = ["dog","cat","ox","duck","frog"];
+
+//  pets.splice(1,2);
+
+// console.log(pets);
+
+// // Q8
+
+// var pets = ["dog","cat","ox","duck","frog","flea"];
+
+//  var reducedpets=pets.slice(3,5);
+
+// console.log(reducedpets);
